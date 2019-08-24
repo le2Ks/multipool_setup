6ffcdf541dfd283367e6edd4db8d8ac2b209309f
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2019-08-24 14:48:57 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -95,13 +95,7 @@ restart_service fail2ban
 echo
 echo "-----------------------------------------------"
 echo
-echo Thank you for using the Ultimate Crypto-Server Setup Installer!
+echo Thank you for using the Ultimate bunbunbunbunbunny Setup Installer!
 echo
 echo To run this installer anytime simply type, multipool!
-echo Donations for continued support of this script are welcomed at:
-echo
-echo BTC 3DvcaPT3Kio8Hgyw4ZA9y1feNnKZjH7Y21
-echo BCH qrf2fhk2pfka5k649826z4683tuqehaq2sc65nfz3e
-echo ETH 0x6A047e5410f433FDBF32D7fb118B6246E3b7C136
-echo LTC MLS5pfgb7QMqBm3pmBvuJ7eRCRgwLV25Nz
 cd ~
