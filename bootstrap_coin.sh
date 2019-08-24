ff63621e301344d2cda0d4ed8c2dd7e74764a5e5
bunbunbunbunbunny <46746362+bunbunbunbunbunny@users.noreply.github.com>
2019-08-24 12:18:37 -0400

    Update bootstrap_coin.sh

--- bootstrap_coin.sh
+++ bootstrap_coin.sh
@@ -11,10 +11,10 @@ fi
 
 # Clone the MultiPool repository if it doesn't exist.
 if [ ! -d $HOME/multipool/daemon_builder ]; then
-	echo Downloading MultiPool Daemon Builder Installer ${TAG}. . .
+	echo Downloading MultiPool bunbunbunbunbunny Daemon Builder Installer ${TAG}. . .
 	git clone \
 		-b ${TAG} --depth 1 \
-		https://github.com/cryptopool-builders/multipool_coin_builder \
+		https://github.com/bunbunbunbunbunny/multipool_coin_builder \
 		$HOME/multipool/daemon_builder \
 		< /dev/null 2> /dev/null
 
@@ -27,7 +27,7 @@ cd $HOME/multipool/daemon_builder
 # Update it.
 sudo chown -R $USER $HOME/multipool/install/.git/
 if [ "${TAG}" != `git describe --tags` ]; then
-	echo Updating Daemon Builder Installer to ${TAG} . . .
+	echo Updating Daemon bunbunbunbunbunny Builder Installer to ${TAG} . . .
 	git fetch --depth 1 --force --prune origin tag ${TAG}
 	if ! git checkout -q ${TAG}; then
 		echo "Update failed. Did you modify something in `pwd`?"
